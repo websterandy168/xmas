9a72ae26bdc26ba18c5f95b94a5a3cef70a65ecd
websterandy168 <websterandy168@gmail.com>
2023-05-24 16:11:42 -0400

    works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+
+    
+   let  refrane = ["a partridge in a pair tree", "two turtle-doves", "three French Hens", "four calling birds", "FIVE GOLDEN RINGS", "six geese a-laying", "seven swans a-swimming", "eight maids a-milking", "nine ladies dancing", "ten lords a-leaping", "eleven pipers piping", "twelve drummers drumming"
+   ];
+   for day in 0..12{
+        println!("On the {}th Day of Christmas my true love gave to me", day+1);
+        for i in (0..day+1).rev(){
+            if i == 1{
+                println!("And a")
+            }
+            println!("{}", refrane[i])
+
+        }
+        
+   }
+
 }
